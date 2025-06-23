ca98426900b750a204de26ebb3cf1410e18cabd2
joshtin2505 <justincastro2505@gmail.com>
2025-06-22 21:33:50 -0500

    feat: add Supabase client and integrate into the application
    - Added Supabase configuration variables to config.ts
    - Created supabaseClient.ts to initialize Supabase client
    - Updated index.ts to export supabaseClient
    - Created layout.astro for consistent page structure
    - Refactored index.astro to use MainLayout
    - Added adLogin page with MainLayout
    - Introduced main.css for styling with Tailwind and global styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
-import db from "@astrojs/db";
+import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel({}),
-  integrations: [db()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
