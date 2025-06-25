a596c5038250e222b2530848037d6049b6b791f5
joshtin2505 <justincastro2505@gmail.com>
2025-06-25 07:28:25 -0500

    feat: update API client to use environment variables and enhance error handling in getWishList function

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -1,9 +1,10 @@
+import { API_TOKEN, API_URL } from "astro:env/server";
 import axios from "axios";
 export default axios.create({
-  baseURL: process.env.API_URL,
+  baseURL: API_URL || "http://localhost:1337",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
-    Authorization: `Bearer ${process.env.API_TOKEN || ""}`,
+    Authorization: `Bearer ${API_TOKEN || ""}`,
   },
 });
--- src/lib/api/wish.ts
+++ src/lib/api/wish.ts
@@ -1,17 +1,40 @@
+import { isAxiosError } from "axios";
 import api from "./client";
-export const getWishList = async () => {
+import type { StrapiResponse, Wish } from "./types";
+export async function getWishList(): Promise<StrapiResponse<Wish>> {
   const getPopulate = (index: number, name: string, item: string) =>
     `populate[${item}][fields][${index}]=${name}`;
 
   const thumbnailNImageFields = ["url", "name", "alternativeText", "caption"];
   const populateThumbnaliQuery = thumbnailNImageFields
-    .map((field, index) => getPopulate(index, field, "thumbnail"))
+    .map((field, index) => getPopulate(index, field, "thumbnali"))
     .join("&");
   const populateImageQuery = thumbnailNImageFields
     .map((field, index) => getPopulate(index, field, "images"))
     .join("&");
-  const response = await api.get(
-    `/api/wishes?${populateImageQuery}&${populateThumbnaliQuery}`
-  );
-  return response.data;
-};
+  try {
+    const { data } = await api.get<StrapiResponse<Wish>>(
+      `/api/wishes?${populateThumbnaliQuery}&${populateImageQuery}`
+    );
+    return data;
+  } catch (error) {
+    console.log(error);
+    if (isAxiosError(error)) {
+      throw new Error(
+        error.response?.data?.message || "Error fetching wish list",
+        {
+          cause: {
+            status: 500,
+            origin: "Query Wish List",
+          },
+        }
+      );
+    }
+    throw new Error("Error fetching wish list", {
+      cause: {
+        status: 500,
+        origin: "Query Wish List",
+      },
+    });
+  }
+}
